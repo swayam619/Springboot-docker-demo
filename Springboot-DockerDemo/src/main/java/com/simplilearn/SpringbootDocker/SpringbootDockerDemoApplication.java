5b9b73a186138c1e125418c373defbb31706458a
swayam619 <88153005+swayam619@users.noreply.github.com>
2022-02-11 13:36:06 +0530

    Add files via upload

---
+++ Springboot-DockerDemo/src/main/java/com/simplilearn/SpringbootDocker/SpringbootDockerDemoApplication.java
@@ -0,0 +1,24 @@
+package com.simplilearn.SpringbootDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerDemoApplication {
+	
+	@GetMapping("/Welcome")
+	public String Welcome() {
+		
+		return("Spring Boot Docker Demo");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+	}
+
+}
